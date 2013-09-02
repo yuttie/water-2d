348134c9ac8845c83cd6935f7a47c745f4fffe08
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-09-03 00:04:34 +0900

    Add support for 64bit CPUs to cpuidutil.c.

--- cpuidutil.c
+++ cpuidutil.c
@@ -12,84 +12,84 @@
 #include <stdbool.h>
 #include "cpuidutil.h"
 
+#define IS_64BIT_CPU defined(__x86_64__) || defined(_M_AMD64) || defined (_M_X64)
+
+
+inline bool CheckFeature(unsigned int i);
+
+
 /* CPUID命令の有無確認関数 */
 bool CheckCPUID() {
     bool b;
     asm volatile (
-        "pushfl\n"
-        "pushfl\n"
-        "popl %%eax\n"
-        "movl %%eax, %%ecx\n"
-        "xorl $0x200000, %%eax\n"
-        "pushl %%eax\n"
-        "popfl\n"
-        "pushfl\n"
-        "popl %%eax\n"
-        "popfl\n"
-        "cmpl %%ecx, %%eax\n"
-        "jz false\n"
-    "true:\n"
-        "movb $0x1, %0\n"
-        "jmp exit\n"
-    "false:\n"
-        "movb $0x0, %0\n"
-    "exit:\n"
-        : "=r" (b)
+#if IS_64BIT_CPU
+        "pushfq\n\t"
+        "pushfq\n\t"
+        "popq %%rax\n\t"
+        "movq %%rax, %%rcx\n\t"
+        "xorq $0x200000, %%rax\n\t"
+        "pushq %%rax\n\t"
+        "popfq\n\t"
+        "pushfq\n\t"
+        "popq %%rax\n\t"
+        "popfq\n\t"
+        "cmpq %%rcx, %%rax\n\t"
+#else
+        "pushfl\n\t"
+        "pushfl\n\t"
+        "popl %%eax\n\t"
+        "movl %%eax, %%ecx\n\t"
+        "xorl $0x200000, %%eax\n\t"
+        "pushl %%eax\n\t"
+        "popfl\n\t"
+        "pushfl\n\t"
+        "popl %%eax\n\t"
+        "popfl\n\t"
+        "cmpl %%ecx, %%eax\n\t"
+#endif
+        "jz false\n\t"
+    "true:\n\t"
+        "movb $0x1, %[result]\n\t"
+        "jmp exit\n\t"
+    "false:\n\t"
+        "movb $0x0, %[result]\n\t"
+    "exit:\n\t"
+        : [result] "=r"(b)
     );
     return b;
 }
 
 /* CPUID命令で得られるEDXレジスタを返す関数 */
 unsigned int GetEDX() {
-    unsigned int edx = 0;
+    unsigned int x = 1;
     asm volatile (
-        "movl $0x1, %%eax\n"
-        "cpuid\n"
-        "movl %%edx, %0\n"
-        : "=r" (edx)
+        "cpuid\n\t"
+        : "=d" (x)
+        : "a" (x)
+        : "%ebx", "%ecx"
     );
-    return edx;
+    return x;
+}
+
+/* featureの有無確認関数 */
+bool CheckFeature(unsigned int i) {
+    bool x = 1;
+    asm volatile (
+        "cpuid\n\t"
+        "btl %[i], %%edx\n\t"
+        "setc %[result]\n\t"
+        : [result] "=r"(x)
+        : "a"(x), [i] "r"(i)
+        : "%ebx", "%ecx", "%edx"
+    );
+    return x;
 }
 
 /* MMX命令の有無確認関数 */
-bool CheckMMX() {
-    bool b = 0;
-    asm volatile (
-        "movl $0x1, %%eax\n"
-        "cpuid\n"
-        "bt $23, %%edx\n"
-        "setc %0\n"
-        : "=r" (b)
-    );
-    return b;
-}
+bool CheckMMX() { return CheckFeature(23); }
 
 /* SSE命令の有無確認関数 */
-bool CheckSSE() {
-    bool b;
-    asm volatile (
-        "movl $0x1, %%eax\n"
-        "cpuid\n"
-        "bt $25, %%edx\n"
-        "setc %0\n"
-        : "=r" (b)
-    );
-    return b;
-}
+bool CheckSSE() { return CheckFeature(25); }
 
 /* SSE2命令の有無確認関数 */
-bool CheckSSE2() {
-    bool b;
-    asm volatile (
-        "movl $0x1, %%eax\n"
-        "cpuid\n"
-        "bt $26, %%edx\n"
-        "setc %0\n"
-        : "=r" (b)
-    );
-    return b;
-}
-
-
-
-
+bool CheckSSE2() { return CheckFeature(26); }
